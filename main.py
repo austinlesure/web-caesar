8a8c3884a1ee7f680a0d9c6124573eea74ea16d0
Austin J.O. LeSure <austin.lesure@protonmail.com>
2017-06-06 22:32:18 -0700

    Added string formatting to the code. Encrypted message now shows up in textarea.

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ page_header = '''
         <head>
             <title>Web Caesar</title>
             <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
         </head>
         <body>
@@ -32,9 +32,9 @@ page_header = '''
 page_form = '''
             <form method='POST'>
                 <label for='rotate'>Rotate by:</label>
-                <input type='number' name='rotate' id='rotate' />
+                <input type='number' name='rotate' id='rotate' value='{0}'/>
                 <br />
-                <textarea name='message' id='message'></textarea>
+                <textarea name='message' id='message'>{1}</textarea>
                 <br />
                 <input type="submit" value="Ceasar!">
             </form>
@@ -50,10 +50,10 @@ def encrypt_page():
     rot = int(request.form['rotate'])
     message = request.form['message']
     secret_message = rotate_string(message, rot)
-    return '<h1>' + secret_message + '</h1>'
+    return page_header + page_form.format(rot, secret_message) + page_footer
 
 @app.route("/")
 def index():
-    return page_header + page_form + page_footer
+    return page_header + page_form.format(0, '') + page_footer
 
 app.run()
